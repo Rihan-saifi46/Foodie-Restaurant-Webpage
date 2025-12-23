bf98c7a20df8b7f2da17ca0172ae4f590aed17db
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-23 22:40:33 +0530

    d

--- Script.js
+++ Script.js
@@ -22,3 +22,21 @@ for (let i = 0; i < navbarLinks.length; i++) {
   });
 }
 
+
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
